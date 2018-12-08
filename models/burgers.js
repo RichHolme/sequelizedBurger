906f34c5892cd52d127e1047a5754cb6a3aad362
Rich <rchrdholme@gmail.com>
2018-12-08 15:38:19 -0500

    working sequelize

--- models/burgers.js
+++ models/burgers.js
@@ -1,7 +1,9 @@
 module.exports = function(sequelize, DataTypes) {
   var Burgers = sequelize.define("Burgers", {
-    burger_nae: DataTypes.STRING,
-    devoured: DataTypes.BOOLEAN
-  });
+    burger_name: DataTypes.STRING,
+    devoured: DataTypes.BOOLEAN,
+  }, {
+	  timestamps: false
+	});
   return Burgers;
 };
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,57 +4,55 @@ module.exports = function(app) {
 
   // GET route for getting all of the todos
   app.get("/", function(req, res) {
-    // findAll returns all entries for a table when used with no options
-    db.Burgers.findAll({}).then(function(dbBurgers) {
-      // We have access to the todos as an argument inside of the callback function
-      // res.json(dbBurgers);
-    console.log(dbBurgers)
- 	  res.render("index", dbBurgers);
+   db.Burgers.findAll({}).then(function(dbBurgers) {
+      // console.log(dbBurgers)
+      let burgerObj = {};
+      let burgers = [];
+      for (var i = 0; i < dbBurgers.length; i++){
+        let burger = {};
+        burger.burger_name = dbBurgers[i].dataValues.burger_name
+        burger.devoured = dbBurgers[i].dataValues.devoured
+        burger.id =  dbBurgers[i].dataValues.id
+
+        burgers.push(burger)
+      }
+
+      console.log(burgers)
+      burgerObj.burgers = burgers
+      console.log('route hit')
+      res.render("index", burgerObj);
     });
   });
 
-  // // POST route for saving a new todo
-  // app.post("/api/todos", function(req, res) {
-  //   // create takes an argument of an object describing the item we want to
-  //   // insert into our table. In this case we just we pass in an object with a text
-  //   // and complete property
-  //   db.Todo.create({
-  //     text: req.body.text,
-  //     complete: req.body.complete
-  //   }).then(function(dbTodo) {
-  //     // We have access to the new todo as an argument inside of the callback function
-  //     res.json(dbTodo);
-  //   });
-  // });
+  app.put("/api/burgers/:id", function(req, res) {
+      console.log(req.body)
+     db.Burgers.update({
+        devoured: req.body.devoured
+      }, {
+        where: {
+          id: req.params.id
+        }
+      }).then(function(dbBurgers) {
+        location.reload();
+      })
+      .catch(function(err) {
+       
+        res.json(err);
+      });
+  });
 
-  // // DELETE route for deleting todos. We can get the id of the todo to be deleted from
-  // // req.params.id
-  // app.delete("/api/todos/:id", function(req, res) {
-  //   // We just have to specify which todo we want to destroy with "where"
-  //   db.Todo.destroy({
-  //     where: {
-  //       id: req.params.id
-  //     }
-  //   }).then(function(dbTodo) {
-  //     res.json(dbTodo);
-  //   });
+  app.post("/api/burgers", function(req, res) {
 
-  // });
+      db.Burgers.create({
+        burger_name: req.body.name,
+        devoured: false
+      }).then(function(dbTodo) {
 
-  // // PUT route for updating todos. We can get the updated todo data from req.body
-  // app.put("/api/todos", function(req, res) {
-  //   // Update takes in an object describing the properties we want to update, and
-  //   // we use where to describe which objects we want to update
-  //   db.Todo.update({
-  //     text: req.body.text,
-  //     complete: req.body.complete
-  //   }, {
-  //     where: {
-  //       id: req.body.id
-  //     }
-  //   }).then(function(dbTodo) {
-  //     res.json(dbTodo);
-  //   });
-  // });
-
+        location.reload();
+      })
+      .catch(function(err) {
+       
+        res.json(err);
+      });
+  });
 };
